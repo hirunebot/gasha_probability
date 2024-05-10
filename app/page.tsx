d95cb62491d408179f2a5b92e5b0a78022ee880a
hirunebot <130118395+hirunebot@users.noreply.github.com>
2024-05-10 14:13:53 +0900

    add: ページのNumberFieldにidを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+"use client";
 import { NoFallbackError } from "next/dist/server/base-server";
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import InputAdornment from '@mui/material/InputAdornment';
 import NumberField from "./NumberField";
 
 
@@ -11,17 +9,15 @@ export default function Home() {
       <h1 className="px-4 py-5 bg-zinc-300 font-bold">ガシャ確率計算機</h1>
       <div className="px-4 py-5">test</div>
 
-      <NumberField label="所持している石" unit="個" />
-      <NumberField label="10連ガシャに必要な石" unit="個" />
-      <NumberField label="排出率" unit="%" />
-      <NumberField label="希望個数" unit="個" />
+      <NumberField id="stones" label="所持している石" unit="個" />
+      <NumberField id="stonesForMultiSummon" label="10連ガシャに必要な石" unit="個" />
+      <NumberField id="summonRate" label="排出率" unit="%" />
+      <NumberField id="desiredNum" label="希望個数" unit="個" />
 
-      <div className="px-4">
-        ~連可能
-      </div>
-      <div className="px-4">
-        ~個以上出る確率は~%
-      </div>
+      <h1 className="text-center py-10">
+        ~連可
+        <br />~個以上出る確率は~%
+      </h1>
     </main>
   );
 }
