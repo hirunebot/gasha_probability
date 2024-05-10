d195cba22866ed641eac786c3330b1bee80f309d
hirunebot <130118395+hirunebot@users.noreply.github.com>
2024-05-10 14:12:28 +0900

    update: NumberFieldがidを受け取れるよう修正

--- app/NumberField.tsx
+++ app/NumberField.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
+import { useState } from "react";
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 
 interface NumberFieldProps {
+  id: string;
   label: string;
   unit: string;
 }
 
-const NumberField: React.FC<NumberFieldProps> = ({ label,unit }) => {
+const NumberField: React.FC<NumberFieldProps> = ({ id,label,unit }) => {
+  const ids = "outlined-number" + {id}
+  const [value, setValue] = useState<number>(0)
   return (
     <div className="px-4">
       <span>{label}</span>
       <TextField 
         className="m-3" 
-        id="outlined-number" 
+        id={ids}
         type="number" 
         variant="outlined" 
         InputProps={{
           endAdornment: <InputAdornment position="end">{unit}</InputAdornment>,
         }}
+        onChange={(event) => setValue(Number(event.target.value))}
       />
+      <p>{value}</p>
     </div>
   );
 }
