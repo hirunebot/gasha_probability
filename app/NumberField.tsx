cc9ec2071c41fbec479a1da6d2edb2f8fb3a6cea
hirunebot <130118395+hirunebot@users.noreply.github.com>
2024-05-11 22:55:56 +0900

    update: NumberFieldに入れた値をpageが受け取るようにする

--- app/NumberField.tsx
+++ app/NumberField.tsx
@@ -7,27 +7,31 @@ interface NumberFieldProps {
   id: string;
   label: string;
   unit: string;
-}
+  func: (value: number) => void;
+} 
 
-const NumberField: React.FC<NumberFieldProps> = ({ id,label,unit }) => {
-  const ids = "outlined-number" + {id}
+const NumberField: React.FC<NumberFieldProps> = (props) => {
+  const ids = "outlined-number" + props.id
   const [value, setValue] = useState<number>(0)
   return (
     <div className="px-4">
-      <span>{label}</span>
+      <span>{props.label}</span>
       <TextField 
         className="m-3" 
         id={ids}
         type="number" 
         variant="outlined" 
         InputProps={{
-          endAdornment: <InputAdornment position="end">{unit}</InputAdornment>,
+          endAdornment: <InputAdornment position="end">{props.unit}</InputAdornment>,
         }}
-        onChange={(event) => setValue(Number(event.target.value))}
+        onChange={(event) => {
+          setValue(Number(event.target.value))
+          props.func(Number(event.target.value))
+        }}
       />
-      <p>{value}</p>
     </div>
   );
+  // eが入力できてしまうので、そこの対策をしたい
 }
 
 export default NumberField;
